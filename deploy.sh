0f37a6ba871096734b20c96d322b949490098ded
SeakKimhour <seakkimhor20@kit.edu.kh>
2023-08-03 12:45:20 +0700

    pre deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-set 
+set -e
+echo "Reatart all commit"
+git restore .
+echo "pulling code"
+git pull
+
+echo "stop all service on docker"
 docker compose down
+
+echo "clean all the server"
 docker system prune -af
+
+echo "start all service"
+docker compose up -d
+
+echo "Completed deploying" 
